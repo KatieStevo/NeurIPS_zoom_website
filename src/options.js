0425e68e0eadaf8f6736d37d11a0e2423f3e52fe
Katie Stevo <47699064+KatieStevo@users.noreply.github.com>
2021-08-24 21:45:04 -0400

    Update options.js

--- src/options.js
+++ src/options.js
@@ -53,7 +53,7 @@ export default {
    * The additional scale factor when grabbing the image.
    * @type {number}
    */
-  scaleExtra: 4.0,
+  scaleExtra: 3.0,
 
   /**
    * How much scrolling it takes before closing out.
